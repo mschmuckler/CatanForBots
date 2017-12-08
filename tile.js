b4d18b7f6062add7ed035b5842f4542220309834
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-12-08 13:25:08 -0500

    Complete tile node connections for setting up board

--- tile.js
+++ tile.js
@@ -66,13 +66,64 @@ class Board {
   
   // call after creating Tiles
   connectNeighborTiles() {
+    // W = i - 1, null if 0, 3, 7, 12, 16
+    // E = i + 1, null if 2, 6, 11, 15, 18
+    // NW = i - 4 if 2nd || 5th row,
+    //      i - 5 if 3rd || 4th row,
+    //      null if 0, 1, 2, 3, 7
+    // NE = i - 3 if 2nd || 5th row,
+    //      i - 4 if 3rd || 4th row,
+    //      null if 0, 1, 2, 6, 11
+    // SW = i + 3 if 1nd || 4th row,
+    //      i + 4 if 2rd || 3th row,
+    //      null if 7, 12, 16, 17 18
+    // SE = i + 4 if 1nd || 4th row,
+    //      i + 5 if 2rd || 3th row,
+    //      null if 11, 15, 16, 17, 18
+    
+    const westNulls = [0, 3, 7, 12, 16];
+    const eastNulls = [2, 6, 11, 15, 18];
+    const nwNulls = [0, 1, 2, 3, 7];
+    const neNulls = [0, 1, 2, 6, 11];
+    const swNulls = [7, 12, 16, 17, 18];
+    const seNulls = [11, 15, 16, 17, 18];
+    const firstRow = [0, 1, 2];
+    const secondRow = [3, 4, 5, 6];
+    const thirdRow = [7, 8, 9, 10, 11];
+    const fourthRow = [12, 13, 14, 15];
+    const fifthRow = [16, 17, 18];
+    
     for (let i = 0; i < this.tiles.length; i++) {
-      switch (i) {
-        case 0:
-          this.tiles[i].neighbors.e = this.tiles[1];
-          this.tiles[i].neighbors.sw = this.tiles[3];
-          this.tiles[i].neighbors.se = this.tiles[4];
-          break;
+      if (!westNulls.includes(i)) {
+        this.tiles[i].neighbors.w = this.tiles[i - 1];
+      }
+      
+      if (!eastNulls.includes(i)) {
+        this.tiles[i].neighbors.e = this.tiles[i + 1];
+      }
+      
+      if (!nwNulls.includes(i)) {
+        this.tiles[i].neighbors.nw = (secondRow.includes(i) || fifthRow.includes(i)) ?
+        this.tiles[i - 4] :
+        this.tiles[i - 5];
+      }
+      
+      if (!neNulls.includes(i)) {
+        this.tiles[i].neighbors.ne = (secondRow.includes(i) || fifthRow.includes(i)) ?
+        this.tiles[i - 3] :
+        this.tiles[i - 4];
+      }
+      
+      if (!swNulls.includes(i)) {
+        this.tiles[i].neighbors.sw = (firstRow.includes(i) || fourthRow.includes(i)) ?
+        this.tiles[i + 3] :
+        this.tiles[i + 4];
+      }
+      
+      if (!seNulls.includes(i)) {
+        this.tiles[i].neighbors.se = (firstRow.includes(i) || fourthRow.includes(i)) ?
+        this.tiles[i + 4] :
+        this.tiles[i + 5];
       }
     }
   }

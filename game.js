9f07f4c23ec798547ae430a51c89c3be5fb15677
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-12-10 09:50:34 -0800

    Refactor mainGameLoop

--- game.js
+++ game.js
@@ -3,7 +3,10 @@ const Board = require('./board.js');
 class Game {
   constructor(...players) { 
     this.board = new Board();
-    this.players = players
+    this.players = players;
+    this.currentPlayerIndex = 0;
+    // otherPlayers is every other not-current player
+    this.otherPlayers = this.players.slice(1);
   }
 
   setupBoard() {
@@ -22,29 +25,43 @@ class Game {
     // Distribute initial resources
   }
   
-  // mainGameLoop() {
-  //   let playerHasWon = false;
-  //   let currentPlayer = 0;
-  // 
-  //   this.setupBoard();
-  //   this.setupInitialSettlements();
-  // 
-  //   while(!playerHasWon) {
-  //     this.rollDiceAndDistributeResources();
-  //     this.players[currentPlayer].tradingPhase();
-  //     this.players[currentPlayer].playDevCardPhase();      
-  //     this.players[currentPlayer].buyDevCardPhase();      
-  //     this.players[currentPlayer].buildingPhase();
-  // 
-  //     playerHasWon = this.checkForVictory();
-  // 
-  //     if (currentPlayer === this.players.length - 1) {
-  //       currentPlayer = 0;
-  //     } else {
-  //       currentPlayer++;
-  //     }
-  //   }
-  // }
+  currentPlayer() {
+    return this.players[this.currentPlayerIndex];
+  }
+  
+  updateCurrentPlayerIndex() {
+    if (this.currentPlayerIndex === this.players.length - 1) {
+      this.currentPlayerIndex = 0;
+    } else {
+      this.currentPlayerIndex++;
+    }
+  }
+  
+  updateOtherPlayers() {
+    this.otherPlayers = this.players.slice(0, this.currentPlayerIndex).concat(this.players.slice(this.currentPlayerIndex + 1));
+  }
+  
+  takeTurn() {
+    this.rollDiceAndDistributeResources();
+    this.currentPlayer().tradingPhase(this);
+    this.currentPlayer().playDevCardPhase(this);
+    this.currentPlayer().buyDevCardPhase(this);
+    this.currentPlayer().buildingPhase(this);
+  }
+  
+  mainGameLoop() {
+    let playerHasWon = false;
+  
+    this.setupBoard();
+    this.setupInitialSettlements();
+  
+    while(!playerHasWon) {
+      this.takeTurn();
+      this.updateCurrentPlayerIndex();
+      this.updateOtherPlayers();
+      playerHasWon = this.checkForVictory();
+    }
+  }
 }
 
 temp = new Game('msm', 'lat', 'xor', 'pyt')

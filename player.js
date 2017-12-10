bc6b430f25a6d4871d47b7a5ef00744c2541db6c
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-12-10 10:35:49 -0800

    Remove player.js comment

--- player.js
+++ player.js
@@ -1,10 +1,3 @@
-// Player should never ask for STDIN, design is for automatic bots.
-// For example, Player object may receive a call to its 'chooseToBuild' function,
-// and it should return either true with a set of tile coordinates 
-// and specifications (settlement/city/road), or false.
-//
-// It is up to the bot's designer to implement a decision making process.
-
 module.exports = class Player {
   constructor() {
     this.hand = [];
